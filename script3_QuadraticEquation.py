6d05a6e0217fcc6075dd14f274e2a022a313c2de
ApoSTal17 <zes.kotov2013@yandex.ru>
2022-12-13 17:41:46 +0000

    commit5

--- script3_QuadraticEquation.py
+++ script3_QuadraticEquation.py
@@ -9,6 +9,7 @@ c = float(input("Введите коэффициент c: "))
 discriminant = b ** 2 - 4 * a * c
 print(f"Дискриминант D = {discriminant}")
 
+
 if discriminant > 0:
     x1 = (-b + math.sqrt(discriminant)) / (2 * a)
     x2 = (-b - math.sqrt(discriminant)) / (2 * a)
